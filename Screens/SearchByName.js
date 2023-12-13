d6acdb4f093cf08c9c1b1cf82f9965826344122f
Fernando-Albiero <fernando.w.albiero@gmail.com>
2023-12-13 13:49:11 -0300

    Added activity indicator loading.

--- Screens/SearchByName.js
+++ Screens/SearchByName.js
@@ -1,4 +1,10 @@
-import { View, Image, TextInput, Button } from 'react-native';
+import {
+   View,
+   Image,
+   TextInput,
+   Button,
+   ActivityIndicator,
+} from 'react-native';
 import { useState } from 'react';
 
 export default function SearchByName() {
@@ -6,6 +12,7 @@ export default function SearchByName() {
    const [uri, setUri] = useState(
       'https://static.vecteezy.com/system/resources/previews/005/337/799/non_2x/icon-image-not-found-free-vector.jpg'
    );
+   const [loading, setLoading] = useState(false);
 
    const options = {
       method: 'GET',
@@ -16,6 +23,8 @@ export default function SearchByName() {
    };
 
    const handleSearch = async () => {
+      setLoading(true);
+
       if (cardName != '') {
          await fetch(
             `https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/${cardName}`,
@@ -24,7 +33,7 @@ export default function SearchByName() {
             .then((response) => response.json())
             .then((data) => {
                if (data.error) {
-                  setUrl(
+                  setUri(
                      'https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg'
                   );
                } else {
@@ -45,15 +54,22 @@ export default function SearchByName() {
       } else {
          alert('Please, inform the card name.');
       }
+
+      setLoading(false);
    };
 
    return (
       <View style={{ flex: 1, alignItems: 'center' }}>
-         <Image
-            style={{ width: '90%', height: '80%' }}
-            source={{ uri: uri }}
-            resizeMode="contain"
-         />
+         {loading ? (
+            <ActivityIndicator size={30} color="black" />
+         ) : (
+            <Image
+               style={{ width: '90%', height: '80%' }}
+               source={{ uri: uri }}
+               resizeMode="contain"
+            />
+         )}
+
          <TextInput
             style={{ borderWidth: 1, width: 300, marginBottom: 20 }}
             onChangeText={(name) => setCardName(name)}
