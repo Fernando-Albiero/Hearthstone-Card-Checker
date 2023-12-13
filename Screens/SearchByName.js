9647ce614cda274f0b1245c11a856b2facf9115e
Fernando-Albiero <fernando.w.albiero@gmail.com>
2023-12-12 23:54:29 -0300

    Implementation of card image request from hearthstone api.

--- Screens/SearchByName.js
+++ Screens/SearchByName.js
@@ -7,7 +7,45 @@ export default function SearchByName() {
       'https://static.vecteezy.com/system/resources/previews/005/337/799/non_2x/icon-image-not-found-free-vector.jpg'
    );
 
-   const handleSearch = () => {};
+   const options = {
+      method: 'GET',
+      headers: {
+         'X-RapidAPI-Key': '81935cf82dmsh5cbfb766a872ce7p16553cjsnec4be953734c',
+         'X-RapidAPI-Host': 'omgvamp-hearthstone-v1.p.rapidapi.com',
+      },
+   };
+
+   const handleSearch = async () => {
+      if (cardName != '') {
+         await fetch(
+            `https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/${cardName}`,
+            options
+         )
+            .then((response) => response.json())
+            .then((data) => {
+               if (data.error) {
+                  setUrl(
+                     'https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg'
+                  );
+               } else {
+                  //Cartas lendárias possuem mais de uma imagem. A correta é sempre a última.
+                  var imagesUri = [];
+                  for (let i = 0; i < data.length; i++) {
+                     if (data[i].hasOwnProperty('img')) {
+                        imagesUri.push(data[i].img);
+                     }
+                  }
+
+                  if (imagesUri.length > 0) {
+                     setUri(imagesUri[imagesUri.length - 1]);
+                  }
+               }
+            })
+            .catch((error) => alert(error));
+      } else {
+         alert('Please, inform the card name.');
+      }
+   };
 
    return (
       <View style={{ flex: 1, alignItems: 'center' }}>
