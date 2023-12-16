92794344b92d7c628e62730b21c3d0415795639e
Fernando-Albiero <fernando.w.albiero@gmail.com>
2023-12-16 16:26:06 -0300

    Added initial requests

--- Screens/SearchByMana.js
+++ Screens/SearchByMana.js
@@ -1,9 +1,12 @@
-import { View, FlatList, Image, TouchableOpacity } from 'react-native';
+import { View, FlatList, Image, TouchableOpacity, ActivityIndicator } from 'react-native';
 import styles from '../Styles/SearchByManaStyle';
 import { useState } from 'react';
+import axios from 'axios';
 
 export default function SearchByMana() {
    const [selected, setSelected] = useState(undefined);
+   const [loading, setLoading] = useState(false);
+   const [cardsUri, setCardsUri] = useState([]);
 
    const costs = [
       {id:0, uri: [require('../assets/0Unselected.png'), require('../assets/0Selected.png')]},
@@ -24,44 +27,103 @@ export default function SearchByMana() {
 
       if(item.id == 10){
          return (
-            <TouchableOpacity onPress={ () =>setSelected(item.id) }>
+            <TouchableOpacity onPress={ () =>{
+               setSelected(item.id); 
+               handleSearch(item)
+            } }>
                <Image 
                   source={image}
-                  style={{width: 50, height:30, resizeMode: 'contain', backgroundColor: 'green'}}>
+                  style={{width: 50, height:30, resizeMode: 'contain'}}>
                </Image>
             </TouchableOpacity>
          )
       }
       else{
          return (
-            <TouchableOpacity onPress={ () => setSelected(item.id) }>
+            <TouchableOpacity onPress={ () =>{
+               setSelected(item.id); 
+               handleSearch(item)
+            } }>
                <Image 
                   source={image}
-                  style={{width: 30, height:30, resizeMode: 'contain', backgroundColor: 'green'}}>
+                  style={{width: 30, height:30, resizeMode: 'contain'}}>
                </Image>
             </TouchableOpacity>
          )
       }
    }
 
-   const handleSearch = () => {
+   const handleSearch = async (item) => {
+      setLoading(true);
       
+      const options = {
+         method: 'GET',
+         params: {
+            cost: item.id,
+            collectible: '1'
+         },
+         headers: {
+           'X-RapidAPI-Key': '81935cf82dmsh5cbfb766a872ce7p16553cjsnec4be953734c',
+           'X-RapidAPI-Host': 'omgvamp-hearthstone-v1.p.rapidapi.com'
+         }
+       };
+
+       try {
+         //Do the request to hearthstone API.
+         const response = await axios.request('https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/classes/Death%20Knight', options);
+         const data = response.data;
+
+         var imagesUri = [];
+
+         //Extract all images found.
+         for(let i=0; i<data.length; i++){
+            if(data[i].hasOwnProperty('img')){
+               imagesUri.push(data[i].img);
+            }
+         }
+
+         setCardsUri(imagesUri);
+      }
+      catch(error){
+         alert(error);
+      }
+
+      setLoading(false);
    }
 
+   const handleCards = (item) => {
+      return (
+         <TouchableOpacity 
+            onPress={ () => {} }>
+            <Image
+               style={{ width: 300, height: 300, resizeMode:'contain'}}
+               source={{ uri: item }}
+            />
+         </TouchableOpacity>
+      );
+   }
 
    return (
       <View style={ styles.container }>
          
          <FlatList
+            style={{backgroundColor: 'green', flexGrow: 0, height: '6%'}}
             horizontal={true}
             data={costs}
             renderItem={({item}) => handleCosts(item)}
          />
-         {/*<FlatList
-            style={ styles.list }
-            data={cardsUri}
-            renderItem={({item}) => handleCards(item)}
-         /> */}
+         {
+            loading ?
+            <View style={{ width: '100%', height: '94%', backgroundColor: 'yellow', justifyContent:'center', alignItems: 'center' }}>
+               <ActivityIndicator size={40} color='black'></ActivityIndicator>
+            </View>
+            :
+            <FlatList
+               style={{ width: '100%', height: '94%', backgroundColor: 'lightblue'}}
+               data={cardsUri}
+               renderItem={({item}) => handleCards(item)}
+            />
+         }
       </View>
       )
    }
--- Styles/SearchByManaStyle.js
+++ Styles/SearchByManaStyle.js
@@ -3,10 +3,9 @@ import { StyleSheet } from 'react-native'
 const styles = StyleSheet.create({
    container:{
       flex: 1,
-      backgroundColor: 'pink',
-      alignItems: 'center'
+      alignItems: 'center',
+      backgroundColor: 'pink'
    },
-
 })
 
 export default styles;
